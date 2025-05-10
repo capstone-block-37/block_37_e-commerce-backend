07b8279d6987417af353aedb4ca9eca63cd407c5
Daren Morreale <daren@givelegacy.com>
2025-05-10 12:23:30 -0500

    clean up

--- server/seed.js
+++ server/seed.js
@@ -30,19 +30,19 @@ const seed = async () => {
     createProduct(
       "Phone",
       "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio",
-      "https://unsplash.com/photos/gray-rotary-telephone-on-brown-table-NKKvASHfrG4",
+      "https://images.unsplash.com/photo-1535882743347-fa16a8c3e48f?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       79.99
     ),
     createProduct(
       "Watch",
       "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio",
-      "https://unsplash.com/photos/a-watch-sitting-on-top-of-a-black-leather-belt-Tk7mHxJUA6I",
-      1299.0
+      "https://images.unsplash.com/photo-1623391306881-7bdb8f98f738?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      1299.99
     ),
     createProduct(
       "Computer",
       "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio",
-      "https://unsplash.com/photos/a-computer-monitor-sitting-on-top-of-a-desk-uqdEKKUhPfQ",
+      "https://images.unsplash.com/photo-1691480195680-144318cfa695?q=80&w=2500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       2899.99
     ),
   ]);
