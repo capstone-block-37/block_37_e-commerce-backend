7ba8e1dc8ee65ac685a1890e82b58be13f6fe93b
Daren Morreale <daren@givelegacy.com>
2025-05-13 20:22:25 -0500

    updates

--- server/seed.js
+++ server/seed.js
@@ -43,7 +43,7 @@ const seed = async () => {
       "Computer",
       "Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio",
       "https://images.unsplash.com/photo-1691480195680-144318cfa695?q=80&w=2500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      2899.99
+      28998.99
     ),
   ]);
   console.log("users and products created");
